d272680dd577801acaa32dbc5179a312458ce40e
Thomas Povinelli <tompov227@gmail.com>
2020-11-07 22:10:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Poll Site
+
+This is a simple node webapp for creating and distributing polls quickly. 
+
+You can create a poll with a free response text area or a poll with fixed multiple choice answers
+
+Anyone can submit a vote or a response 
+
+You can limit how many times a specific IP is allowed to submit using the `.env` file (implemented with `express-rate-limit`).
+*Note that this is disabled unless `process.env.environment=='production'`
+
+Results of all polls are viewable by everyone
+
+Polls never close 
+
+Inspired by @shiffman and his attempts to make a live poll overlay 
+
+Uses: `express`, `express-rate-limit`, `nedb`, `pug` and `materializecss`
---
+++ server.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const fs = require('fs');
+const path = require('path');
+const pug = require('pug');
+
+const db = require('nedb');
+const store = new db({
+    filename: "database.txt",
+    autoload: true
+});
+
+const app = express();
+app.use(express.json());
+
+const environment = process.env.environment;
+
+// slow mode for production use
+if (environment == 'production') {
+    app.use(rateLimit({
+        max: process.env.RATE_LIMIT_SUBMISSIONS || 1,
+        windowMs: process.env.RATE_LIMIT_WINDOW_MILLIS || (1000 * 60 * 1) // 1 minute 
+    }));
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'private', 'index.html'));
+});
+
+app.get('/poll/new', (req, res) => {
+    res.sendFile(path.join(__dirname, 'private', 'create.html'));
+})
+
+app.post('/poll/create', (req, res) => {
+    let created = Date.now();
+    let id = (created + Math.round(Math.random() * 100000000)) % (2 << 48);
+    store.insert({
+        ...req.body,
+        created: created,
+        pollID: id,
+        responses: []
+    }, (err, doc) => {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                err: err
+            })
+        } else {
+            res.json({
+                pollID: id,
+                created: created,
+                success: true
+            });
+        }
+    });
+});
+
+const pollRenderer = pug.compileFile(path.join(__dirname, 'private', 'poll.pug'))
+const resultRenderer = pug.compileFile(path.join(__dirname, 'private', 'results.pug'))
+
+
+app.get('/poll/:id', (req, res) => {
+    let id = req.params.id;
+    store.findOne({
+        pollID: Number(id) || -1
+    }, (err, doc) => {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                err: err
+            })
+        } else {
+            res.send(pollRenderer({
+                ...doc
+            }))
+        }
+    });
+});
+
+app.post('/poll/submit', (req, res) => {
+    let pollID = req.body.pollID;
+    console.log(req.body);
+    let response = {
+        ...req.body.response
+    }
+
+    store.update({
+        pollID: Number(pollID)
+    }, {
+        $push: {
+            responses: response
+        }
+    }, {
+        upsert: false
+    }, (err, num) => {
+        if (err || num > 1) {
+            res.status(500).json({
+                success: false,
+                error: err || 'Number was greater than 1'
+            })
+        } else {
+            res.json({
+                success: true,
+                response: response,
+                pollID: pollID
+            });
+        }
+    })
+
+});
+
+app.get('/poll/results/:id', (req, res) => {
+    store.findOne({
+        pollID: Number(req.params.id)
+    }, (err, doc) => {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                err: err
+            })
+        } else {
+            res.send(resultRenderer(doc));
+        }
+    })
+})
+
+app.listen(8000);
